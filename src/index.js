8867fa657d6c01aed014ba61b7347688af3c2189
cooper.chen <cooper.chen@ffcnn238.local>
2018-09-09 13:26:15 +0800

    generator settings without react

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// Exported static site renderer:
+module.exports = function render(locals, callback) {
+    callback(null, '<html>Hello!</html>');
+};
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
+var data = require('./data.js');
+
+module.exports = {
+  entry: './src',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    libraryTarget: 'umd', // this is super important,
+    globalObject: "this" // https://github.com/markdalgleish/static-site-generator-webpack-plugin/issues/130
+  },
+  module: {
+    rules: [
+        {
+            test: /\.js/,
+            use: [{ loader: 'babel-loader' }],
+            include: __dirname + '/src',
+        },
+        {
+            test: /\.css/,
+            loader: ExtractTextPlugin.extract('css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'),
+            include: __dirname + '/src'
+        }
+    ],
+  },
+  plugins: [
+    new StaticSiteGeneratorPlugin('main', data.routes, data),
+    new ExtractTextPlugin("styles.css"),
+  ]
+};
